6e3290825ccbb7c35543305e8e79b61a8db0d1cf
rashmi <Administrator@10.0.0.193>
2016-03-14 11:56:48 -0500

    first commit

---
+++ com/src/main/java/MavenApp/com/App.java
@@ -0,0 +1,11 @@
+package MavenApp.com;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World11!" );
+    }
+}
