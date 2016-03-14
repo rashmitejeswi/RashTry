1477f468ddb2a9c9a472555a8305f725020cb09c
Administrator <Administrator@10.0.0.193>
2016-03-14 13:13:07 -0500

    rashmi

--- com/src/main/java/MavenApp/com/App.java
+++ com/src/main/java/MavenApp/com/App.java
@@ -1,11 +1,31 @@
 package MavenApp.com;
-
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello how r u" );
+    	  try {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://gmail.com");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("Email")).sendKeys("bandaryqa");
+        driver.findElement(By.id("next")).click();
+        driver.findElement(By.id("Passwd")).sendKeys("Deetya@2012");
+        driver.findElement(By.id("signIn")).click();
+      
+			driver.wait(2000);
+			driver.close();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
+        
     }
 }
